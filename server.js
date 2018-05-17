d16dc2e401d5318aec8e89409cabbd8239b4f9ce
unknown <jjernigan1065@gmail.com>
2018-05-17 00:14:53 -0400

    finished the model, controller, and started on the layouts, connects to server but getting an error when trying to connect the database

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const exphbs = require('express-handlebars');
+
+const PORT = process.env.PORT || 8081;
+
+const app = express();
+
+app.use(express.static('public'));
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+const routes = require('./controllers/burger_controllers');
+
+app.use(routes);
+
+app.listen(PORT, () => {
+    console.log(`Server listening on: http://localhost:${PORT}`);
+});
